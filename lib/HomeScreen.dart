6e9be8f3113cacfa684eb09ff73ec220bfdea534
Rahma-Hedaia <64587581+Rahma-Hedaia@users.noreply.github.com>
2022-09-03 12:56:51 +0200

    created home screen created quran tab finished quran tab and sura details

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:islami/HadethTab.dart';
+import 'package:islami/quran/QuranTab.dart';
+import 'package:islami/RadioTab.dart';
+import 'package:islami/TasbehTab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget{
 static String routeName ='Home screen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+int currentIndex=0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return Stack(
+      children: [
+        Image.asset('assets/images/background.png',width: double.infinity,height: double.infinity,fit: BoxFit.fill),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('اسلامى',style: Theme.of(context).textTheme.headline1,),
+            centerTitle: true,
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: Theme.of(context).primaryColor
+            ),
+            child: BottomNavigationBar(
+              selectedItemColor: Colors.black,
+              unselectedItemColor: Colors.white,
+              currentIndex: currentIndex,
+              onTap: (index){
+                currentIndex=index;
+                setState(() {
+                });
+              },
+              backgroundColor: Theme.of(context).primaryColor,
+              items: [
+                BottomNavigationBarItem(icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                  label: 'الاذاعة'),
+                BottomNavigationBarItem(icon: ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+                    label: 'سبحه'),
+                BottomNavigationBarItem(icon: ImageIcon(AssetImage('assets/images/ic_quran.png')),
+                    label: 'حديث'),
+                BottomNavigationBarItem(icon: ImageIcon(AssetImage('assets/images/ic_moshaf.png')),
+                    label: 'قرآن'),
+              ],
+            ),
+          ),
+          body: tabs[currentIndex],
+        )
+      ],
     );
   }
+  List<Widget> tabs=[
+    RadioTab(),TasbehTab(),HadethTab(),QuranTab()
+  ];
 }
---
+++ lib/sura_details/ItemVerse.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:islami/sura_details/SuraDetails.dart';
+
+class ItemVerse extends StatelessWidget {
+  static String routeName='Itemverse';
+String text;
+ItemVerse(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 12),
+      child: Text(text,style: Theme.of(context).textTheme.subtitle1,textAlign: TextAlign.center),
+    );
+  }
+}
+
